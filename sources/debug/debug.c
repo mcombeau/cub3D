7a3f0043c0e9217a6cd2dc46ace979ea9a150464
mcombeau <mcombeau@student.42.fr>
2023-01-28 13:20:45 +0100

    add debug info about player

--- sources/debug/debug.c
+++ sources/debug/debug.c
@@ -3,7 +3,7 @@
 void	debug_display_map(t_data *data)
 {
 	int	i;
-	
+
 	i = 0;
 	while (data->map.map[i])
 	{
@@ -11,7 +11,12 @@ void	debug_display_map(t_data *data)
 		i++;
 	}
 }
+
 void	debug_display_data(t_data *data)
 {
 	debug_display_map(data);
+	printf("Map nb_line: %d\n", data->map.nb_line);
+	printf("\nPlayer position: x = %d, y = %d\n", data->map.p_x, data->map.p_y);
+	printf("Player direction: %c\n", data->map.p_direction);
+	printf("Index end of map: %d\n", data->map.index_end_of_map);
 }
