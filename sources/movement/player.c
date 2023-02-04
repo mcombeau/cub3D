a67516380e9b15f1f62e91e1d88de47eeba9573f
mcombeau <mcombeau@student.42.fr>
2023-02-04 16:00:39 +0100

    set player.pos x and y at init

--- sources/movement/player.c
+++ sources/movement/player.c
@@ -10,5 +10,6 @@ void	init_player_direction(t_data *data)
 		data->player.view_angle = data->mapinfo.south;
 	else if (data->player.direction == 'W')
 		data->player.view_angle = data->mapinfo.west;
+	data->player.pos_x = data->player.tile_x;
+	data->player.pos_y = data->player.tile_y;
 }
-
