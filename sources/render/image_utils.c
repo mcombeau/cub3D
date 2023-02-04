f5165ccdfb2582c1b156ed0f6616ba38258fda70
mcombeau <mcombeau@student.42.fr>
2023-02-04 15:07:25 +0100

    norm

--- sources/render/image_utils.c
+++ sources/render/image_utils.c
@@ -5,22 +5,18 @@ void	set_image_pixel(t_img *image, int x, int y, int color)
 	int	pixel;
 
 	pixel = y * image->size_line + x * (image->pixel_bits / 8);
-	if (image->endian == 1)        // Most significant (Alpha) byte first
-    {
-        image->addr[pixel + 0] = (color >> 24);
-        image->addr[pixel + 1] = (color >> 16) & 0xFF;
-        image->addr[pixel + 2] = (color >> 8) & 0xFF;
-        image->addr[pixel + 3] = (color) & 0xFF;
-    }
-    else if (image->endian == 0)   // Least significant (Blue) byte first
-    {
-        image->addr[pixel + 0] = (color) & 0xFF;
-        image->addr[pixel + 1] = (color >> 8) & 0xFF;
-        image->addr[pixel + 2] = (color >> 16) & 0xFF;
-        image->addr[pixel + 3] = (color >> 24);
-    }
-	/* image->addr[index] = color; */
-	/* image->addr[index + 1] = color >> 8; */
-	/* image->addr[index + 2] = color >> 16; */
-	/* image->addr[index + 3] = color >> 24; */
+	if (image->endian == 1)
+	{
+		image->addr[pixel + 0] = (color >> 24);
+		image->addr[pixel + 1] = (color >> 16) & 0xFF;
+		image->addr[pixel + 2] = (color >> 8) & 0xFF;
+		image->addr[pixel + 3] = (color) & 0xFF;
+	}
+	else if (image->endian == 0)
+	{
+		image->addr[pixel + 0] = (color) & 0xFF;
+		image->addr[pixel + 1] = (color >> 8) & 0xFF;
+		image->addr[pixel + 2] = (color >> 16) & 0xFF;
+		image->addr[pixel + 3] = (color >> 24);
+	}
 }
